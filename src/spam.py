8f8d955063e001fa2e99d91551c0388a569eb858
yxhung <yxhung@gmail.com>
2015-10-13 09:53:42 +0800

    import configparse to set run part files

--- src/spam.py
+++ src/spam.py
@@ -4,13 +4,8 @@ import os
 import csv
 import time
 
-DIR_WHOSCALL = '/home/yaxuan/mmnas2/yaxuan/whoscall'
-os.chdir(DIR_WHOSCALL)
-
-
-# os.chdir('/Users/yxhung/Dropbox/workspace/whoscall_local')
-
-
+DIRWHOSCALL = '/home/yaxuan/mmnas2/yaxuan/whoscall'
+os.chdir(DIRWHOSCALL)
 
 def listdir_fullpath(d):
     return [os.path.join(d, f) for f in os.listdir(d)]
@@ -20,6 +15,21 @@ def listdir_pattern(d, pattern):
     return [os.path.join(d, f) for f in os.listdir(d) if pattern in f]
 
 
+def detect_not_spam(line):
+    l = line.split(',')
+    if (len(l) != 17):
+        return False
+    ctr = l[11]
+    local_num = l[10]
+    remote_num = l[2]
+    if spams.get(ctr) is None:
+        return True
+    if spams[ctr].get(local_num) is None and spams[ctr].get(remote_num) is None:
+        return True
+    else:
+        return False
+
+# 1 create the spam dic
 spams = {}
 spam_dirs = listdir_fullpath('call_spam')
 try:
@@ -40,61 +50,53 @@ except IOError:
     print('data missing')
 
 
-def detect_not_spam(line):
-    l = line.split(',')
-    if (len(l) != 17):
-        return False
-    ctr = l[11]
-    local_num = l[10]
-    remote_num = l[2]
-    if spams.get(ctr) is None:
-        return True
-    if spams[ctr].get(local_num) is None and spams[ctr].get(remote_num) is None:
-        return True
-    else:
-        return False
 
 
+# setting the part
 calls = os.listdir('call_all')
 calls.sort()
 
-o1t1 = 0
-o1t2 = 0
-o1t3 = 0
-o1t4 = 0
+import configparser
+config = configparser.ConfigParser()
+config.read('/home/yaxuan/mmnas2/yaxuan/python/social_connection/conf/config')
 
-o2t1 = 0
-o2t2 = 0
-o2t3 = 0
+o1t1 = config['DEFAULT']['o1t1']
+o1t2 = config['DEFAULT']['o1t2']
+o1t3 = config['DEFAULT']['o1t3']
+o1t4 = config['DEFAULT']['o1t4']
 
-o3t1 = 1
-o3t2 = 0
-o3t3 = 0
+o2t1 = config['DEFAULT']['o2t1']
+o2t2 = config['DEFAULT']['o2t2']
+o2t3 = config['DEFAULT']['o2t3']
+
+o3t1 = config['DEFAULT']['o3t1']
+o3t2 = config['DEFAULT']['o3t2']
+o3t3 = config['DEFAULT']['o3t3']
 
 if o1t1:
-    calls = calls[4:10]
+    partCalls = calls[0:10]
 if o1t2:
-    calls = calls[11:20]
+    partCalls = calls[10:20]
 if o1t3:
-    calls = calls[21:30]
+    partCalls = calls[20:30]
 if o1t4:
-    calls = calls[32:40]
+    partCalls = calls[30:40]
 
 if o2t1:
-    calls = calls[41:50]
+    partCalls = calls[40:50]
 if o2t2:
-    calls = calls[52:60]
+    partCalls = calls[50:60]
 if o2t3:
-    calls = calls[61:70]
+    partCalls = calls[60:70]
 
 if o3t1:
-    calls = calls[71:80]
+    partCalls = calls[70:80]
 if o3t2:
-    calls = calls[82:90]
+    partCalls = calls[80:90]
 if o3t3:
-    calls = calls[91:99]
+    partCalls = calls[90:99]
 
-for call in calls:
+for call in partCalls:
 
     #start = time.time()
     with open('call_all/' + call) as calldata:
